bd19ed2d77aa85af1611ff3c6ded1a70ad131a27
Prachi Gautam <prgautam@isystango.com>
2021-01-29 20:06:19 +0530

    fixing head issue

--- variables.js
+++ variables.js
@@ -21,7 +21,7 @@ for (let i = 0; i < 5; i++) {
 
 //JavaScript const in a for loop
 
-
+console.log("JavaScript const in a for loop");
 let scores = [75, 80, 95];
 for (const score of scores) {
     console.log(score);
